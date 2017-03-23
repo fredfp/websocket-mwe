eb9b49211399a02ea7a881f131511c3ec724e303
Frederic Arno <fredericarno@gmail.com>
2017-03-23 16:59:28 +0800

    Include potential fix

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -31,6 +31,14 @@ class HomeController @Inject()(implicit system: ActorSystem, mat: Materializer) 
   def webSocketWithActorGood = WebSocket.accept[String, String] { r =>
     ActorFlow.actorRef(out => StreamActor.props(true, out, mat))
   }
+
+  def fixedWebSocketWithActorBad = WebSocket.accept[String, String] { r =>
+    MyActorFlow.actorRef(out => StreamActor.props(false, out, mat))
+  }
+
+  def fixedWebSocketWithActorGood = WebSocket.accept[String, String] { r =>
+    MyActorFlow.actorRef(out => StreamActor.props(true, out, mat))
+  }
 }
 
 object StreamActor {
@@ -54,5 +62,54 @@ case class StreamActor(good: Boolean, out: ActorRef, materializer: Materializer)
         out ! Status.Success(())
       else
         self ! PoisonPill
+  }
+
+  override def postStop ={
+    println("actor stopped")
+    super.postStop
+  }
+}
+
+object MyActorFlow {
+  import akka.actor._
+  import akka.stream.{ Materializer, OverflowStrategy }
+  import akka.stream.scaladsl.{ Sink, Keep, Source, Flow }
+
+  /**
+   * Create a flow that is handled by an actor.
+   *
+   * Messages can be sent downstream by sending them to the actor passed into the props function.  This actor meets
+   * the contract of the actor returned by [[akka.stream.scaladsl.Source.actorRef]].
+   *
+   * The props function should return the props for an actor to handle the flow. This actor will be created using the
+   * passed in [[akka.actor.ActorRefFactory]]. Each message received will be sent to the actor - there is no back pressure,
+   * if the actor is unable to process the messages, they will queue up in the actors mailbox. The upstream can be
+   * cancelled by the actor terminating itself.
+   *
+   * @param props A function that creates the props for actor to handle the flow.
+   * @param bufferSize The maximum number of elements to buffer.
+   * @param overflowStrategy The strategy for how to handle a buffer overflow.
+   */
+  def actorRef[In, Out](props: ActorRef => Props, bufferSize: Int = 16, overflowStrategy: OverflowStrategy = OverflowStrategy.dropNew)(implicit factory: ActorRefFactory, mat: Materializer): Flow[In, Out, _] = {
+
+    val (outActor, publisher) = Source.actorRef[Out](bufferSize, overflowStrategy)
+      .toMat(Sink.asPublisher(false))(Keep.both).run()
+
+    Flow.fromSinkAndSource(
+      Sink.actorRef(factory.actorOf(Props(new Actor {
+        val flowActor = context.watch(context.actorOf(props(outActor), "flowActor"))
+
+        def receive = {
+          case Status.Success(_) | Status.Failure(_) => flowActor ! PoisonPill
+          case Terminated(_)                         => outActor ! Status.Success(())
+          case other                                 => flowActor ! other
+        }
+
+        override def supervisorStrategy = OneForOneStrategy() {
+          case _ => SupervisorStrategy.Stop
+        }
+      })), Status.Success(())),
+      Source.fromPublisher(publisher)
+    )
   }
 }
